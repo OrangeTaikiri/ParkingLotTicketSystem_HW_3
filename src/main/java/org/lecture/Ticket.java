b50bf865055a147edf5fa36a06413e00749d82c8
orangetaikiri <Rafche@gmail.com>
2025-10-06 21:08:43 +0200

    Add ticket lifecycle management with detailed state transitions and .idea gitignore rules

--- src/main/java/org/lecture/Ticket.java
+++ src/main/java/org/lecture/Ticket.java
@@ -1,11 +1,27 @@
 package org.lecture;
 
+/**
+ * Represents a ticket with a unique number that can transition through various states.
+ *
+ * The ticket goes through the following states:
+ * ISSUED, PAID, VALIDATED, CLOSED, and CANCELLED. It transitions between these states
+ * based on specific actions such as paying, validating, closing, or canceling the ticket.
+ */
 public class Ticket {
 
     String ticketNumber;
 
     TicketStatusEnumeration status;
 
+    /**
+     * Constructs a new Ticket with a given ticket number.
+     *
+     * The ticket number is automatically converted to uppercase, and the initial
+     * status of the ticket is set to ISSUED. A message is printed to indicate the
+     * creation and status of the ticket using the printStateTransaction method.
+     *
+     * @param ticketNumber the identifier of the ticket to be created
+     */
     public Ticket(String ticketNumber) {
         this.ticketNumber = ticketNumber.toUpperCase();
         status = TicketStatusEnumeration.ISSUED;
@@ -13,6 +29,14 @@ public class Ticket {
     }
 
 
+    /**
+     * Pays the ticket if it is currently in the ISSUED state.
+     *
+     * If the ticket's status is ISSUED, the status is updated to PAID, and a success message
+     * is printed using the printStateTransaction method.
+     * If the ticket is not in the ISSUED state, the payment will not be processed, and
+     * an error message will be printed.
+     */
     public void payTicket() {
         if (status == TicketStatusEnumeration.ISSUED) {
             this.status = TicketStatusEnumeration.PAID;
@@ -22,6 +46,14 @@ public class Ticket {
         }
     }
 
+    /**
+     * Validates the ticket if it is currently in the PAID state.
+     *
+     * If the ticket is in the PAID state, its status will be updated to VALIDATED
+     * and a success message will be printed using the printStateTransaction method.
+     * If the ticket is in any other state, validation will not be allowed and
+     * an error message will be printed.
+     */
     public void validateTicket() {
         if (status == TicketStatusEnumeration.PAID) {
             this.status = TicketStatusEnumeration.VALIDATED;
@@ -31,6 +63,14 @@ public class Ticket {
         }
     }
 
+    /**
+     * Closes the ticket if it is currently in the VALIDATED state.
+     *
+     * If the ticket is in the VALIDATED state, its status will be updated to CLOSED
+     * and a success message will be printed using the printStateTransaction method.
+     * If the ticket is in any other state, the closure will not be allowed and
+     * an error message will be printed.
+     */
     public void closeTicket() {
         if (status == TicketStatusEnumeration.VALIDATED) {
             this.status = TicketStatusEnumeration.CLOSED;
@@ -41,6 +81,14 @@ public class Ticket {
 
     }
 
+    /**
+     * Cancels the ticket if it is currently in the ISSUED state.
+     *
+     * If the ticket is in the ISSUED state, its status will be updated to CANCELLED
+     * and a success message will be printed using the printStateTransaction method.
+     * If the ticket is in any other state, the cancellation will not be allowed
+     * and an error message will be printed.
+     */
     public void cancelTicket() {
         if (status == TicketStatusEnumeration.ISSUED) {
             this.status = TicketStatusEnumeration.CANCELLED;
@@ -50,16 +98,23 @@ public class Ticket {
         }
     }
 
+    /**
+     * Prints the current status of the ticket along with its ticket number.
+     * The status includes information such as whether the ticket is issued,
+     * paid, validated, closed, or canceled.
+     */
     public void printStatus() {
         System.out.println("Ticket " + ticketNumber + " is currently " + status + ".");
     }
 
+    /**
+     * Prints the result of a state transition operation for a ticket.
+     *
+     * @param stateString the name of the state to which the ticket is transitioning
+     * @param success     a boolean indicating whether the transition was successful
+     */
     private void printStateTransaction(String stateString, boolean success) {
-        if(success){
-            System.out.printf("Ticket %s has been %s %n", this.ticketNumber,  stateString);
-        } else  {
-            System.out.printf("Ticket %s cannot be %s at this stage %n", this.ticketNumber,  stateString);
-        }
+        System.out.printf(success ? "Ticket %s has been %s %n" : "Ticket %s cannot be %s at this stage %n", this.ticketNumber,  stateString);
     }
 
 
--- src/main/java/org/lecture/TicketStatusEnumeration.java
+++ src/main/java/org/lecture/TicketStatusEnumeration.java
@@ -1,5 +1,19 @@
 package org.lecture;
 
+/**
+ * Enumeration representing the various possible statuses of a ticket.
+ *
+ * A ticket can transition through the following statuses:
+ *
+ * - ISSUED: The initial state of the ticket when it is created.
+ * - PAID: The state of the ticket after it has been paid.
+ * - VALIDATED: The state of the ticket after it has been validated.
+ * - CLOSED: The state of the ticket after it has been closed.
+ * - CANCELLED: The state of the ticket after it has been cancelled.
+ *
+ * Instances of this enumeration are used to track and manage the state of a ticket
+ * as it transitions through its lifecycle.
+ */
 public enum TicketStatusEnumeration {
 
     ISSUED,
